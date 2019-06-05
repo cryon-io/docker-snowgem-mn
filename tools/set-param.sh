9918f4fe08e300c14093b9ef85bfca2f8c79c0af
cryi <cryi@tutanota.com>
2019-06-05 16:53:44 +0200

    escaping sed for params

--- tools/set-param.sh
+++ tools/set-param.sh
@@ -22,17 +22,19 @@ BASEDIR=$(dirname "$0")
 
 PARAM=$(echo "$1" | sed "s/=.*//")
 VALUE=$(echo "$1" | sed "s/[^>]*=//")
+# escape value for sed
+VALUE_FOR_SED=$(echo "$VALUE" | sed -e 's/[\/&]/\\&/g')
 
 case $PARAM in
     ip)
         case $VALUE in 
             *:*)
-                TEMP=$(sed "s/externalip=.*/externalip=[$VALUE]:16113/g" "$BASEDIR/../data/snowgem/snowgem.conf")
-                TEMP=$(printf "%s" "$TEMP" | sed "s/masternodeaddr=.*/masternodeaddr=[$VALUE]:16113/g")
+                TEMP=$(sed "s/externalip=.*/externalip=[$VALUE_FOR_SED]:16113/g" "$BASEDIR/../data/snowgem/snowgem.conf")
+                TEMP=$(printf "%s" "$TEMP" | sed "s/masternodeaddr=.*/masternodeaddr=[$VALUE_FOR_SED]:16113/g")
             ;;
             *)
-                TEMP=$(sed "s/externalip=.*/externalip=$VALUE:16113/g" "$BASEDIR/../data/snowgem/snowgem.conf")
-                TEMP=$(printf "%s" "$TEMP" | sed "s/masternodeaddr=.*/masternodeaddr=$VALUE:16113/g")
+                TEMP=$(sed "s/externalip=.*/externalip=$VALUE_FOR_SED:16113/g" "$BASEDIR/../data/snowgem/snowgem.conf")
+                TEMP=$(printf "%s" "$TEMP" | sed "s/masternodeaddr=.*/masternodeaddr=$VALUE_FOR_SED:16113/g")
             ;;
         esac
         printf "%s" "$TEMP" > "$BASEDIR/../data/snowgem/snowgem.conf"
@@ -48,18 +50,18 @@ case $PARAM in
         esac
     ;;
     nodeprivkey)
-        TEMP=$(sed "s/masternodeprivkey=.*/masternodeprivkey=$VALUE/g" "$BASEDIR/../data/snowgem/snowgem.conf")
+        TEMP=$(sed "s/masternodeprivkey=.*/masternodeprivkey=$VALUE_FOR_SED/g" "$BASEDIR/../data/snowgem/snowgem.conf")
         printf "%s" "$TEMP" > "$BASEDIR/../data/snowgem/snowgem.conf"
         MN_CONF_PART1=$(awk '{print $1" "$2}' "$BASEDIR/../data/snowgem/masternode.conf")
         MN_CONF_PART2=$(awk '{print $4" "$5}' "$BASEDIR/../data/snowgem/masternode.conf")
         printf "%s %s %s" "$MN_CONF_PART1" "$VALUE" "$MN_CONF_PART2" > "$BASEDIR/../data/snowgem/masternode.conf"
     ;;
     NODE_VERSION) 
-        if grep "NODE_VERSION=" "$BASEDIR/../containers/limits.conf"; then
-            TEMP=$(sed "s/NODE_VERSION=.*/NODE_VERSION=$VALUE/g" "$BASEDIR/../containers/limits.conf")
-            printf "%s" "$TEMP" > "$BASEDIR/../containers/limits.conf"
+        if grep "NODE_VERSION=" "$BASEDIR/../container/limits.conf"; then
+            TEMP=$(sed "s/NODE_VERSION=.*/NODE_VERSION=$VALUE_FOR_SED/g" "$BASEDIR/../container/limits.conf")
+            printf "%s" "$TEMP" > "$BASEDIR/../container/limits.conf"
         else 
-            printf "NODE_VERSION=%s" "$VALUE" >> "$BASEDIR/../containers/limits.conf"
+            printf "\nNODE_VERSION=%s" "$VALUE" >> "$BASEDIR/../container/limits.conf"
         fi
     ;;
     PROJECT)
@@ -68,7 +70,7 @@ case $PARAM in
         printf "%s %s" "$VALUE" "$MN_CONF_PART" > "$BASEDIR/../data/snowgem/masternode.conf"
     ;;
     bootstrap)
-        TEMP=$(sed "s/BOOTSTRAP_URL=.*/BOOTSTRAP_URL=\"$VALUE\"/g" "$BASEDIR/before-start.sh")
+        TEMP=$(sed "s/BOOTSTRAP_URL=.*/BOOTSTRAP_URL=\"$VALUE_FOR_SED\"/g" "$BASEDIR/before-start.sh")
         printf "%s" "$TEMP" > "$BASEDIR/before-start.sh"
     ;;
     txid)
